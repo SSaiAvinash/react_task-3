9f1d07ba1ba5d13d0d25c1446cabd8a1707f2473
saiavinash <saiavinash69@gmail.com>
2020-07-13 21:36:35 +0530

    its commited

---
+++ precache-manifest.6e927ccef6999394841ae353ac8d57ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddd8b6d47076fa609ccea6657ceeb8aa",
+    "url": "/react_task3/index.html"
+  },
+  {
+    "revision": "741c8dc402e9d1c0ee5a",
+    "url": "/react_task3/static/css/main.911cad69.chunk.css"
+  },
+  {
+    "revision": "eb67426b2855e03116ce",
+    "url": "/react_task3/static/js/2.8bf73427.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react_task3/static/js/2.8bf73427.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "741c8dc402e9d1c0ee5a",
+    "url": "/react_task3/static/js/main.a25dfd59.chunk.js"
+  },
+  {
+    "revision": "fe616a7b51086f41d8fe",
+    "url": "/react_task3/static/js/runtime-main.22641516.js"
+  },
+  {
+    "revision": "6f88fc6b60d4b7515f1b0d14efd320a0",
+    "url": "/react_task3/static/media/profile_pic.6f88fc6b.svg"
+  }
+]);
